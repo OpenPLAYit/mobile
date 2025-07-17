d4b0cab2b36f98aaf7bdebe7e1e45e570ac48ad6
onyedkichi23 <ozohenrywk@gmail.com>
2025-07-17 05:04:42 +0100

    chore: Remove unnecessary modules added at first init.

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,19 +1,23 @@
+/** @format */
+
 import { Link, Stack } from "expo-router";
 
+import { Box } from "@/components/ui/box";
 import { Text } from "@/components/ui/text";
-import { Center } from "@/components/ui/center";
 
 export default function NotFoundScreen() {
-  return (
-    <>
-      <Stack.Screen options={{ title: "Oops!" }} />
-      <Center className="flex-1">
-        <Text className="text-secondary-200">This screen doesn't exist.</Text>
+	return (
+		<>
+			<Stack.Screen options={{ title: "Oops!" }} />
+			<Box className="flex-1 items-center justify-center">
+				<Text className="text-secondary-200">
+					This screen doesn't exist.
+				</Text>
 
-        <Link href="/" style={{ marginTop: 10 }}>
-          <Text className="text-primary-500">Go to home screen!</Text>
-        </Link>
-      </Center>
-    </>
-  );
+				<Link href="/" style={{ marginTop: 10 }}>
+					<Text className="text-primary-500">Go to home screen!</Text>
+				</Link>
+			</Box>
+		</>
+	);
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,23 @@
+/** @format */
+
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
+	DarkTheme,
+	DefaultTheme,
+	ThemeProvider,
 } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect, useState } from "react";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
-import { useColorScheme } from "@/components/useColorScheme";
+import { useColorScheme } from "react-native";
 import { Slot } from "expo-router";
 
 import "../global.css";
 
 export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
+	// Catch any errors thrown by the Layout component.
+	ErrorBoundary,
 } from "expo-router";
 
 // export const unstable_settings = {
@@ -27,42 +29,43 @@ export {
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-    ...FontAwesome.font,
-  });
+	const [loaded, error] = useFonts({
+		SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+		...FontAwesome.font,
+	});
 
-  const [styleLoaded, setStyleLoaded] = useState(false);
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
+	const [styleLoaded, setStyleLoaded] = useState(false);
+	// Expo Router uses Error Boundaries to catch errors in the navigation tree.
+	useEffect(() => {
+		if (error) throw error;
+	}, [error]);
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
+	useEffect(() => {
+		if (loaded) {
+			SplashScreen.hideAsync();
+		}
+	}, [loaded]);
 
-  // useLayoutEffect(() => {
-  //   setStyleLoaded(true);
-  // }, [styleLoaded]);
+	// useLayoutEffect(() => {
+	//   setStyleLoaded(true);
+	// }, [styleLoaded]);
 
-  // if (!loaded || !styleLoaded) {
-  //   return null;
-  // }
+	// if (!loaded || !styleLoaded) {
+	//   return null;
+	// }
 
-  return <RootLayoutNav />;
+	return <RootLayoutNav />;
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
+	const colorScheme = useColorScheme();
 
-  return (
-    <GluestackUIProvider mode={colorScheme === "dark" ? "dark" : "light"}>
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        <Slot />
-      </ThemeProvider>
-    </GluestackUIProvider>
-  );
+	return (
+		<GluestackUIProvider mode={colorScheme === "dark" ? "dark" : "light"}>
+			<ThemeProvider
+				value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+				<Slot />
+			</ThemeProvider>
+		</GluestackUIProvider>
+	);
 }
