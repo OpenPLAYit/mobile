0031bb22988938fbe50e5ec6ccedae48f4859d5f
onyedkichi23 <ozohenrywk@gmail.com>
2025-07-17 11:20:18 +0100

    refactor: Implement Slot component for usage with prop slot pattern (Radix UI's Slot).

---
+++ src/types/index.d.ts
@@ -0,0 +1,9 @@
+/** @format */
+import type React from "react";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type ReadableProps<T extends React.ComponentType<any>> = Prettify<
+	React.ComponentProps<T>
+>;
+
+type PlainObject = Record<PropertyKey, unknown>;
