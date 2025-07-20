22da31219b93e727b686ef2c2c85ff22c1538aaf
onyedkichi23 <ozohenrywk@gmail.com>
2025-07-20 19:20:02 +0100

    feat(video): Implement video tab top bar
    Adds a new top bar to the video tab.

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,12 +1,30 @@
 /** @format */
 
 import { Box } from "@/components/ui/box";
-import { Text } from "@/components/ui/text";
+import { Icon } from "@/components/ui/icon";
+import { Image } from "@/components/ui/image";
+import { BRAND_NAME } from "@/constants";
+import { ArrowDownToLine, Search, Trash } from "lucide-react-native";
 
-export default function Video() {
+export default function VideoTab() {
 	return (
-		<Box className="flex-">
-			<Text className="text-blue-800">Home screen</Text>
+		<Box className="flex-1 p-4">
+			{/* top bar */}
+			<Box className="flex-row items-center justify-between">
+				<Image
+					// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+					source={require("@/assets/images/brand/logo-text.png")}
+					alt={`${BRAND_NAME} Logo`}
+					size="none"
+					className="aspect-[881/177] w-48"
+				/>
+
+				<Box className="flex-row items-center gap-4">
+					{[Trash, Search, ArrowDownToLine].map((icon, index) => (
+						<Icon key={index} as={icon} size={"xl"} />
+					))}
+				</Box>
+			</Box>
 		</Box>
 	);
 }
